20969e0e03ce519c99b276555f4780a1b5b676aa
David Oosthuizen <david.oosthuizen@hotmail.com>
2023-08-05 11:28:22 +0800

    John Smilga node and express course intro

--- logger.js
+++ logger.js
@@ -1,6 +1,20 @@
+const EventEmitter = require('events')
+
 var url = 'http://mylogger.io/log';
 
-function log(message) {
-    //send an HTTP request
-    console.log(message);
+class Logger extends EventEmitter {
+    log(message) { //when a function is inside a class it is a method
+        //send an HTTP request
+        console.log(message);
+        // Raise an event
+        this.emit('messageLogged', {id: 1, url: 'http://'});
+    }
 }
+
+
+
+module.exports = Logger;
+
+
+
+
