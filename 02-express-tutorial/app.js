46490f973baf23f7c8cf619fa136bad341355ea2
David Oosthuizen <david.oosthuizen@hotmail.com>
2023-08-15 10:03:23 +0800

    further progress studying html servers in node in course.

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,35 @@
 console.log('Express Tutorial')
+
+const http = require('http')
+const { readFileSync } = require('fs')
+
+// get all files
+const homePage = readFileSync('./index.html')
+
+const server = http.createServer((req, res)=>{
+    const url = req.url
+    // HOME PAGE
+    if( url === '/'){
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write(homePage)
+        res.end()
+    }
+    // ABOUT PAGE
+    else if (url === '/about'){
+        res.writeHead(200, {'content-type':'text/html'})
+        res.write('<h1> About Page </h1>')
+        res.end()
+    }
+    else {
+        res.writeHead(404, {'content-type':'text/html'})
+        res.write('<h1> Page not found </h1>')
+        res.end()
+    }
+    
+})
+
+server.listen(5000)
+
+// console.log(`user hit the server`);
+// console.log(req.method)
+// console.log(req.url)
