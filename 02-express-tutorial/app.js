0fc1eb933b7ed548360848d3f76b6e3911cdc70a
David Oosthuizen <david.oosthuizen@hotmail.com>
2023-08-21 13:04:57 +0800

    continued with course, query string info and middleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,32 +1,48 @@
 const express = require('express')
 const app = express();
-const {products} = require('./data')
+const morgan = require('morgan')
+const logger = require('./logger')
+const authorize = require('./authorize')
+
+// req => middleware => res
+
+// 1. use vs route
+// 2. options - our own / express / third party
+
+// exported to it's own file
+// const logger = (req, res, next) => {
+//     const method = req.method;
+//     const url = req.url;
+//     const time = new Date().getDate()
+//     console.log(method, url, time)
+//     next()
+// app.get('/', logger, (req, res) => {
+//     res.send('Home')
+// })
+// }
+
+app.use(morgan('tiny'))
+//app.use(express.static('./public'))
+//app.use([logger, authorize]) 
+//logger used for path, multiple functions executed in order
 
 app.get('/', (req, res) => {
-    res.send('<h1> Home Page </h1><a href="/api/products">products</a>')
+    res.send('Home')
 })
 
-app.get('/api/products/:productID', (req, res) => {
-    // console.log(req);
-    // console.log(req.params)
-    const {productID} = req.params
-    const singleProduct = products.find((product) => product.id === Number(productID))
-    if(!singleProduct){
-        return res.status(404).send("product does not exist")
-    }
-    res.json(singleProduct)
+app.get('/about', (req, res) => {
+    res.send('About')
 })
 
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-    console.log(req.params)
-    res.send("hello world")
+app.get('/api/products', (req, res) => {
+    res.send('Products')
+})
 
+app.get('/api/items', (req, res) => {
+    console.log(req.user)
+    res.send('Items')
 })
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')
 })
-
-//const newProducts = products.map((product)=>{
-    // const {id, name, image} = product;
-    // return {id, name, image}
