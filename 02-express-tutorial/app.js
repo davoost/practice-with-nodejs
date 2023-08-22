17e7b672c0aff90b8be771c0b124968abc9f30d3
David Oosthuizen <david.oosthuizen@hotmail.com>
2023-08-22 16:30:14 +0800

    finishing the node-js and express-js course, methods and setting up express router

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,48 +1,18 @@
 const express = require('express')
-const app = express();
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
+const app = express()
+const people  = require('./routes/people')
+const auth  = require('./routes/auth')  
 
-// req => middleware => res
+// statis assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
-// 1. use vs route
-// 2. options - our own / express / third party
-
-// exported to it's own file
-// const logger = (req, res, next) => {
-//     const method = req.method;
-//     const url = req.url;
-//     const time = new Date().getDate()
-//     console.log(method, url, time)
-//     next()
-// app.get('/', logger, (req, res) => {
-//     res.send('Home')
-// })
-// }
-
-app.use(morgan('tiny'))
-//app.use(express.static('./public'))
-//app.use([logger, authorize]) 
-//logger used for path, multiple functions executed in order
-
-app.get('/', (req, res) => {
-    res.send('Home')
-})
-
-app.get('/about', (req, res) => {
-    res.send('About')
-})
-
-app.get('/api/products', (req, res) => {
-    res.send('Products')
-})
-
-app.get('/api/items', (req, res) => {
-    console.log(req.user)
-    res.send('Items')
-})
+app.use('/api/people', people, )
+app.use('/login', auth)
 
 app.listen(5000, () => {
-    console.log('Server is listening on port 5000')
+    console.log('Server is listening on port 5000....')
 })
--- my_course_materials/CourseMaterials.js
+++ my_course_materials/CourseMaterials.js
@@ -328,3 +328,78 @@ app.listen(5000, ()=> {
 // //const newProducts = products.map((product)=>{
 //     // const {id, name, image} = product;
 //     // return {id, name, image}
+
+const express = require('express')
+// const app = express();
+// const morgan = require('morgan')
+// const logger = require('./logger')
+// const authorize = require('./authorize')
+
+// // req => middleware => res
+
+// // 1. use vs route
+// // 2. options - our own / express / third party
+
+// // exported to it's own file
+// // const logger = (req, res, next) => {
+// //     const method = req.method;
+// //     const url = req.url;
+// //     const time = new Date().getDate()
+// //     console.log(method, url, time)
+// //     next()
+// // app.get('/', logger, (req, res) => {
+// //     res.send('Home')
+// // })
+// // }
+
+// app.use(morgan('tiny'))
+// //app.use(express.static('./public'))
+// //app.use([logger, authorize]) 
+// //logger used for path, multiple functions executed in order
+
+// app.get('/', (req, res) => {
+//     res.send('Home')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('About')
+// })
+
+// app.get('/api/products', (req, res) => {
+//     res.send('Products')
+// })
+
+// app.get('/api/items', (req, res) => {
+//     console.log(req.user)
+//     res.send('Items')
+// })
+
+// app.listen(5000, () => {
+//     console.log('Server is listening on port 5000')
+// })
+
+// const express = require('express')
+// const app = express()
+// let { people } = require('./data') 
+
+// // statis assets
+// app.use(express.static('./methods-public'))
+// // parse form data
+// app.use(express.urlencoded({ extended: false }))
+
+// app.get('/api/people', (req, res) => {
+//     res.status(200).json({success: true, data: people})
+// })
+
+// app.post('/login', (req, res) => {
+//     console.log(req.body)
+//     const { name } = req.body
+//     if (name){
+//         return res.status(200).send(`Welcome ${name}`)
+//     }
+//     res.status(401).send('Please provide credentials')
+// })
+
+// app.listen(5000, () => {
+//     console.log('Server is listening on port 5000....')
+// })
