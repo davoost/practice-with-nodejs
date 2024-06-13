3ab076127374c0eb8fe0c8755ccc73e6d88ce592
David Oosthuizen <david.oosthuizen@hotmail.com>
2024-06-13 20:02:31 +0100

    finally got back to working on the nodejs tutorial, started the last project in the series and started work on the auth controller.

--- extra-course/06-jobs-api/final/app.js
+++ extra-course/06-jobs-api/final/app.js
@@ -23,7 +23,7 @@ const jobsRouter = require('./routes/jobs');
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
-
+//
 app.set('trust proxy', 1);
 app.use(
   rateLimiter({
@@ -48,7 +48,7 @@ app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
