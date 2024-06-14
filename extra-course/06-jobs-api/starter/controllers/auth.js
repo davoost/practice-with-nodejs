a8b205a214d2ecd8157cc0f3b09187325df92907
David Oosthuizen <david.oosthuizen@hotmail.com>
2024-06-14 21:03:15 +0100

    further work on the nodejs course. This update includes work on setting up json web tokens for creating a hash of the password. This keeps things secure.

--- extra-course/06-jobs-api/starter/controllers/auth.js
+++ extra-course/06-jobs-api/starter/controllers/auth.js
@@ -1,14 +1,39 @@
 
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
+const { BadRequestError, UnauthenticatedError } = require('../errors')
+const jwt = require('jsonwebtoken')
+
 
 const register = async (req, res) => {
+    // const {name, email, password} = req.body
+    // if( !name || !email || !password ){
+    //     throw new BadRequestError('Please provide name, email and password')
+    // }
     const user = await User.create({ ...req.body })
-    res.status(StatusCodes.CREATED).json({ user })
-    // res.send(req.body)
+    const token = user.createJWT()
+    res
+        .status(StatusCodes.CREATED)
+        .json({user: {name: user.name}, token })
 }
 const login = async (req, res) => {
-    res.send('login user')
+    const {email, password} = req.body
+    if(!email || !password){
+        throw new BadRequestError('Please provide email and password')
+    }
+    const user = await User.findOne({email})
+    // check user exists
+    if(!user){
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    // compare password
+    const isPasswordCorrect = await user.comparePassword(password)
+    if(!isPasswordCorrect){
+        throw new UnauthenticatedError('Invalid Password')
+    }
+    const token = user.createJWT();
+    res.status(StatusCodes.OK).json({ user: { name:user.name }, token })
 }
 
 module.exports = {
